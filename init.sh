c759457846e59dbbbf9225b6113da2c8ee1b0f55
bbrtj <bbrtj.pro@gmail.com>
2025-04-25 10:58:11 +0200

    No need for custom brightness anymore

--- init.sh
+++ init.sh
@@ -2,9 +2,8 @@
 
 # Set up environment
 
-doas pkg install git xorg ncurses tmux pkgconf freetype2 fontconfig patch tree universal-ctags htop neofetch xbindkeys stalonetray the_silver_searcher py38-ranger firefox xf86-video-intel slock dmenu intel-backlight wpa_supplicant_gui freefont-ttf libsynaptics slim slim-freebsd-dark-theme pavucontrol xclip thunderbird gimp-app feh file-roller gmp gnome-screenshot bash xcompmgr neovim fusefs-ntfs drm-kmod twemoji-color-font-ttf ja-font-std zh-font-std fonts-indic webcamd v4l-utils v4l_compat calc hs-pandoc
+doas pkg install git xorg ncurses tmux pkgconf freetype2 fontconfig patch tree universal-ctags htop neofetch xbindkeys stalonetray the_silver_searcher py38-ranger firefox xf86-video-intel slock dmenu intel-backlight wpa_supplicant_gui freefont-ttf libsynaptics slim slim-freebsd-dark-theme pavucontrol xclip thunderbird gimp-app feh file-roller gmp gnome-screenshot bash xcompmgr neovim fusefs-ntfs drm-kmod twemoji-color-font-ttf ja-font-std zh-font-std fonts-indic webcamd v4l-utils v4l_compat hs-pandoc
 
-doas cp ~/config/bin/brightness /usr/local/bin/brightness
 ln -s /usr/local/bin/ranger ~/bin/list
 
 git worktree add system-independent
