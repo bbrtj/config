3a8709c6f1c8d5cd953f26cb73d2a1b5676b3ab1
bbrtj <bbrtj.pro@gmail.com>
2025-05-12 15:23:42 +0200

    More config

--- init.sh
+++ init.sh
@@ -6,5 +6,8 @@ git config --global core.excludesfile ~/.gitignore
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+mkdir -p ~/.dzil
+git clone https://github.com/bbrtj/perl-dzil-profiles ~/.dzil/profiles
+
 \curl -L https://install.perlbrew.pl | bash
 
