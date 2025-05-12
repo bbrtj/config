48679f1caadbdf204ea4df384b75c471428688ba
bbrtj <bbrtj.pro@gmail.com>
2025-05-12 17:06:47 +0200

    Add various configs and a script to copy them to their locations

---
+++ slackware/configs/etc$$profile.d$$bsd-games-login-fortune.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Print a fortune cookie for interactive shells:
+
+case $- in
+*i* )  # We're interactive
+  rand=$(($RANDOM % 100))
+  if [ $rand -ge 75 ]; then
+    fortune fortunes fortunes2 linuxcookie | cowsay -f camel
+  echo
+  fi
+  ;;
+esac
+
