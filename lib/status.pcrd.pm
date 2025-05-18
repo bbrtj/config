be8b8426019cde42b2943a310b352ae436b6fba1
bbrtj <bbrtj.pro@gmail.com>
2025-05-18 20:18:20 +0200

    Move locking screen to a separate plugin

--- lib/status.pcrd.pm
+++ lib/status.pcrd.pm
@@ -148,41 +148,6 @@ sub uptime_status
 	return _colsym(COLOR_SELECTED . $uptime_symbol, $uptime);
 }
 
-sub check_lock_screen
-{
-	my ($self, $feature) = @_;
-
-	my $ex = PCRD::Util::try {
-		PCRD::Util::slurp_command($feature->{config}{command}, '-v');
-	};
-
-	return ['command', $ex] unless !$ex;
-	return undef;
-}
-
-sub init_lock_screen
-{
-	my ($self, $feature) = @_;
-
-	# after suspend, system clock will jump forward and $last_timestamp will be
-	# far in the past - so the screen is locked very briefly after resume
-	my $last_timestamp = time;
-	my $timer = IO::Async::Timer::Periodic->new(
-		interval => 60,
-		reschedule => 'skip',
-		on_tick => sub {
-			if (time - $last_timestamp > 60 * 15) {
-				PCRD::Util::slurp_command($feature->{config}{command});
-			}
-
-			$last_timestamp = time;
-		},
-	);
-
-	$timer->start;
-	$self->{pcrd}{loop}->add($timer);
-}
-
 sub check_build_default_line
 {
 	my ($self, $feature) = @_;
@@ -287,16 +252,6 @@ sub set_build_time_line
 sub _build_features
 {
 	return {
-		lock_screen => {
-			desc => 'locks screen after long suspend',
-			mode => 'i',
-			config => {
-				command => {
-					desc => 'command to lock screen',
-					value => 'slock',
-				},
-			},
-		},
 		build_default_line => {
 			desc => 'builds the default line',
 			mode => 'iw',
