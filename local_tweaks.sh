b722b45e77f86d68c4034a49ba0051dae7252971
bbrtj <bbrtj.pro@gmail.com>
2025-06-10 06:51:59 +0200

    Use libinput instead of synaptics

--- local_tweaks.sh
+++ local_tweaks.sh
@@ -1,16 +1,2 @@
-synclient FingerHigh=40
-synclient MaxTapTime=250
-synclient MaxTapMove=20
-synclient MaxSpeed=2
-synclient TapButton2=0
-synclient TapButton3=2
-synclient HorizTwoFingerScroll=1
-synclient PalmDetect=1
-synclient PalmMinWidth=3
-synclient PalmMinZ=30
-synclient AreaLeftEdge=200
-synclient AreaRightEdge=$(($(synclient | perl -ne 'print m{\bRightEdge\s+=\s+(\d+)}')-200))
-synclient AreaTopEdge=100
-
 export DMENU_PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin"
 
