a60bf59cad4abe0ff1514d8a63fd6cc31e697639
bbrtj <bbrtj.pro@gmail.com>
2025-12-18 08:10:19 +0100

    Adjust config

--- local_config.example.sh
+++ local_config.example.sh
@@ -2,16 +2,11 @@
 
 # xhost + local:
 # export DISPLAY=localhost:0
-export WALLPAPER="$HOME/pix/wallpaper.jpg"
 export LC_CTYPE=pl_PL.UTF-8
 export LANG=pl_PL.UTF-8
 
 # fix DWM java apps
 export _JAVA_AWT_WM_NONREPARENTING=1
-
-# libreoffice needs this
-pkill dbus
-export $(dbus-launch)
 
 . ~/config/local_tweaks.sh
 #export DMENU_FAVS="firefox thunderbird ovoplayer calc lazarus dbeaver doublecmd transmission file-roller pavucontrol nm-applet blueman-applet eboard xnethack xadventure gvim gcolor thunar xfce4-clipman fpcupdeluxe"
